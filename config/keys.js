bcdc61ab777d3f10b3986ded0bad102954485483
Follett <clf9008@gmail.com>
2021-03-04 19:45:13 -0700

    updated routes, views and server.js for passport authentication

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb://clf9008:'+ encodeURIComponent('CEXU5186') + '@cluster0.mongodb.net/WhatAreWeEating?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
--- server.js
+++ server.js
@@ -1,22 +1,65 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
-app.set('view-engine', 'ejs')
+const app = express();
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
+// Passport Config
+require('./config/passport')(passport);
 
-app.get('/login', (req, res) => {
-    res.render('login.ejs')
-})
+// DB Config
+const db = require('./config/keys').mongoURI;
 
-app.get('/register', (req, res) => {
-    res.render('register.ejs')
-})
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+      {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+      }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-app.post('/register', (req, res) => {
-    
-})
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
-app.listen(3003)
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
