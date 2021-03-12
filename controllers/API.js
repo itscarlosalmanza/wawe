6dd3683f0ab7c9b2e490d8a94e22d8a34cd335dd
itscarlosalmanza <69559059+itscarlosalmanza@users.noreply.github.com>
2021-03-11 18:54:44 -0700

    switched to AXIOS api call

--- controllers/API.js
+++ controllers/API.js
@@ -1,23 +1,9 @@
-const apiKey = "4fffe504459346578fa3ef8d6daa24ea";
-// import axios from "axios";
+import axios from "axios";
 
 
-function getRandomRecipe(x) {
-    var requestURL = "https://api.spoonacular.com/recipes/random?number=1&apiKey=" + apiKey;
-
-    $.ajax({
-        url: requestURL,
-        method: "GET",
-        success: function (recipeJSON) {
-            console.log(recipeJSON)
-        }
-    })      
-}
-
-//  might need to use axios instead, talk to team.
-// export default {
-//     // Gets all users
-//     getUserRecipes: function() {
-//       return axios.get("https://api.spoonacular.com/recipes/random?number=1&apiKey=4fffe504459346578fa3ef8d6daa24ea");
-//     }
-//   };
+export default {
+    // Gets all users
+    getUserRecipes: function() {
+      return axios.get("https://api.spoonacular.com/recipes/random?number=1&apiKey=4fffe504459346578fa3ef8d6daa24ea");
+    }
+  };
