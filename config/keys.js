974a5af6fd27d4f4a09ec5d4e7ed32d58e164351
Follett <clf9008@gmail.com>
2021-03-16 12:07:01 -0600

    updated

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-dbPassword = 'mongodb://clf9008:'+ encodeURIComponent('clf9008') + '@Cluster0.mongodb.net/WhatAreWeEating?retryWrites=true&w=majority';
-
-module.exports = {
-    mongoURI: dbPassword
-};
--- models/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    userRecipes: require("./userRecipes"),
-    User: require("./User"),
-}
-
-// API.JS MIGHT HAVE TO MOVE TO FRONT-END UTILS FOLDER TO MAKE THE CALL
